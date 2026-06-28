c8459e0ab52cc6d78e70f18fa5f555488b6f7a12
juniorta <ttjrbiz@gmail.com>
2026-06-28 10:19:25 -0400

    adding logic to test images integartion using unsplash

--- next.config.ts
+++ next.config.ts
@@ -57,3 +57,74 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+
+// For testing images with unsplash (remove unsplash in secured domains):
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "*.supabase.co",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "images.unsplash.com", // 1. Added to allow Next.js <Image /> component optimization
+//       },
+//     ],
+//   },
+//   async headers() {
+//     return [
+//       {
+//         source: "/(.*)",
+//         headers: [
+//           {
+//             key: "X-DNS-Prefetch-Control",
+//             value: "on",
+//           },
+//           {
+//             key: "Strict-Transport-Security",
+//             value: "max-age=63072000; includeSubDomains; preload",
+//           },
+//           {
+//             key: "X-Frame-Options",
+//             value: "DENY",
+//           },
+//           {
+//             key: "X-Content-Type-Options",
+//             value: "nosniff",
+//           },
+//           {
+//             key: "Referrer-Policy",
+//             value: "strict-origin-when-cross-origin",
+//           },
+//           {
+//             key: "Permissions-Policy",
+//             value: "camera=(), microphone=(), geolocation=()",
+//           },
+//           {
+//             key: "Content-Security-Policy",
+//             value: [
+//               "default-src 'self'",
+//               "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://js.stripe.com",
+//               "style-src 'self' 'unsafe-inline'",
+//               // 2. Added https://images.unsplash.com to the img-src array below
+//               "img-src 'self' blob: data: https://*.supabase.co https://images.unsplash.com",
+//               "font-src 'self'",
+//               "frame-src https://js.stripe.com https://hooks.stripe.com",
+//               "connect-src 'self' https://*.supabase.co https://api.stripe.com",
+//             ].join("; "),
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+
